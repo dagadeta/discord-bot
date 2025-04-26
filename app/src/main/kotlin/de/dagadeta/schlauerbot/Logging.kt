37513f52b6e7640ad33b97c367cebf44f49afde6
dagadeta <dagadeta@web.de>
2025-04-27 00:53:15 +0200

    Move logger definition

--- app/src/main/kotlin/de/dagadeta/schlauerbot/Logging.kt
+++ app/src/main/kotlin/de/dagadeta/schlauerbot/Logging.kt
@@ -3,9 +3,10 @@ package de.dagadeta.schlauerbot
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.dv8tion.jda.api.JDA
 
-private val logger = KotlinLogging.logger {}
 
 class Logging(private val guild: JDA, private val guildId: Long, private val channelId: Long) {
+    private val logger = KotlinLogging.logger {}
+
     fun log(message: String) {
         logger.info { message }
         sendMessageToDiscordChannelById(guild, guildId, channelId, message)
