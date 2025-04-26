5ee8fd2d38b0c20d4f10af2938bffb5ff6f00972
dagadeta <dagadeta@web.de>
2025-04-26 13:23:24 +0200

    Improve logging

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-echo "Please enter your username on the target host: "
+printf "\nPlease enter your username on the target host: "
 read -r TARGET_USER
-echo "Please enter the hostname or IP address of the target host: "
+printf "\nPlease enter the hostname or IP address of the target host: "
 read -r TARGET_HOST
-TARGET_DIR=/home/$TARGET_USER/discord-bot
+
+set -x
+
+TARGET_DIR=/home/$TARGET_USER/discord-bot/bin
 
 ./gradlew distZip
 # shellcheck disable=SC2029
+ssh "$TARGET_USER@$TARGET_HOST" "screen -XS bot_run quit" || true
 ssh "$TARGET_USER@$TARGET_HOST" "rm -rf $TARGET_DIR && mkdir -p $TARGET_DIR"
 scp app/build/distributions/app.zip "$TARGET_USER@$TARGET_HOST:$TARGET_DIR"
 scp run.sh "$TARGET_USER@$TARGET_HOST:$TARGET_DIR"
@@ -18,4 +22,7 @@ scp config.properties "$TARGET_USER@$TARGET_HOST:$TARGET_DIR"
 ssh "$TARGET_USER@$TARGET_HOST" "cd $TARGET_DIR && unzip app.zip && chmod +x run.sh"
 # shellcheck disable=SC2029
 ssh "$TARGET_USER@$TARGET_HOST" "cd $TARGET_DIR && screen -dmS bot_run ./run.sh"
+
+set +x
+
 echo "Application is running in a detached screen session named 'bot_run'."
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-rm -f out.log err.log
-echo "Starting the app..." | tee -a out.log | tee -a err.log
-app/bin/app >> out.log 2>> err.log
+echo "Starting the app..."
+export JAVA_OPTS="-DLOG_DIR=../logs"
+app/bin/app
