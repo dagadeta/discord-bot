f263f8ed9b986b7c43b822ede9c31ff07e3be340
dagadeta <dagadeta@web.de>
2025-05-08 20:59:37 +0200

    Make the app a spring boot application

--- app/src/main/kotlin/de/dagadeta/schlauerbot/App.kt
+++ app/src/main/kotlin/de/dagadeta/schlauerbot/App.kt
@@ -1,61 +1,12 @@
 package de.dagadeta.schlauerbot
 
-import de.dagadeta.schlauerbot.wordchaingame.DiscordWordChainGame
-import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.requests.GatewayIntent
-import java.io.File
-import java.util.*
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 
-fun main() {
-    val props = Properties()
-    val inputStream = File("config.properties").inputStream()
-    inputStream.use {
-        props.load(inputStream)
-    }
+@SpringBootApplication
+class DiscordBotApplication
 
-    val wordChecker = WiktionaryWordChecker(props.getProperty("dictionary.language"), Logging.INITIAL)
-    val wordChainGame = DiscordWordChainGame(
-        props.getProperty("wordChainGame.channelId").toLong(),
-        props.getProperty("dictionary.language"),
-        wordChecker
-    )
-    val dingDong = DingDongListener()
-
-    val api = JDABuilder
-        .createLight(
-            props.getProperty("bot.token"),
-            GatewayIntent.GUILD_MESSAGES,
-            GatewayIntent.MESSAGE_CONTENT,
-            GatewayIntent.GUILD_MEMBERS
-        )
-        .addEventListeners(
-            dingDong,
-            wordChainGame,
-        )
-        .build()
-
-    api.awaitReady()
-
-    // Initialize Logging and WordChecker with the actual API instance
-    val logging = Logging(
-        api,
-        props.getProperty("logging.guildId").toLong(),
-        props.getProperty("logging.channelId").toLong()
-    )
-
-    // Update the WordChecker instance with the correct logging configuration
-    wordChecker.logger = logging
-
-    logging.log("Bot started")
-    logging.logOnShutdown("Bot stopped")
-
-    configureCommands(api, dingDong, wordChainGame)
-}
-
-fun configureCommands(guild: JDA, vararg commandsProvider: WithSlashCommands) {
-    guild.updateCommands().addCommands(
-        commandsProvider.flatMap(WithSlashCommands::getSlashCommands)
-    ).queue()
+fun main(args: Array<String>) {
+    runApplication<DiscordBotApplication>(*args)
 }
---
+++ app/src/main/kotlin/de/dagadeta/schlauerbot/DiscordBot.kt
@@ -0,0 +1,67 @@
+package de.dagadeta.schlauerbot
+
+import de.dagadeta.schlauerbot.wordchaingame.DiscordWordChainGame
+import jakarta.annotation.PostConstruct
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.requests.GatewayIntent
+import org.springframework.stereotype.Component
+import java.io.File
+import java.util.Properties
+
+@Component
+class DiscordBot {
+
+    @PostConstruct
+    fun startBot() {
+        val props = Properties()
+        val inputStream = File("config.properties").inputStream()
+        inputStream.use {
+            props.load(inputStream)
+        }
+
+        val wordChecker = WiktionaryWordChecker(props.getProperty("dictionary.language"), Logging.INITIAL)
+        val wordChainGame = DiscordWordChainGame(
+            props.getProperty("wordChainGame.channelId").toLong(),
+            props.getProperty("dictionary.language"),
+            wordChecker
+        )
+        val dingDong = DingDongListener()
+
+        val api = JDABuilder
+            .createLight(
+                props.getProperty("bot.token"),
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.GUILD_MEMBERS
+            )
+            .addEventListeners(
+                dingDong,
+                wordChainGame,
+            )
+            .build()
+
+        api.awaitReady()
+
+        // Initialize Logging and WordChecker with the actual API instance
+        val logging = Logging(
+            api,
+            props.getProperty("logging.guildId").toLong(),
+            props.getProperty("logging.channelId").toLong()
+        )
+
+        // Update the WordChecker instance with the correct logging configuration
+        wordChecker.logger = logging
+
+        logging.log("Bot started")
+        logging.logOnShutdown("Bot stopped")
+
+        configureCommands(api, dingDong, wordChainGame)
+    }
+
+    private fun configureCommands(guild: JDA, vararg commandsProvider: WithSlashCommands) {
+        guild.updateCommands().addCommands(
+            commandsProvider.flatMap(WithSlashCommands::getSlashCommands)
+        ).queue()
+    }
+}
