6cb9da3162d039ce6be193cf31676f95e5661bc1
dagadeta <dagadeta@web.de>
2025-05-11 01:28:10 +0200

    Fix database integration tests

--- app/src/test/kotlin/de/dagadeta/schlauerbot/persistance/DatabaseIntegrationTest.kt
+++ app/src/test/kotlin/de/dagadeta/schlauerbot/persistance/DatabaseIntegrationTest.kt
@@ -7,17 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
-// TODO: These tests fail by now. Here are some ideas to fix them:
-//@SpringBootTest
-//@ContextConfiguration(classes = [WordChainGameStateRepository::class, WordChainGameStatePersistenceService::class])
 @AutoConfigureEmbeddedDatabase
 class DatabaseIntegrationTest() {
 
     @Autowired
     lateinit var gameStateRepo: WordChainGameStateRepository
-
-    @Autowired
-    lateinit var persistenceService: WordChainGameStatePersistenceService
 
     @Test
     fun `on empty database nothing is found`() {
@@ -26,6 +20,7 @@ class DatabaseIntegrationTest() {
 
     @Test
     fun `the WordChainGameState can be upserted`() {
+        val persistenceService = WordChainGameStatePersistenceService(gameStateRepo)
         val first = persistenceService.upsert(WordChainGameState(0, false, ""))
         assertThat(first).isEqualTo(WordChainGameState(0, false, ""))
         assertThat(gameStateRepo.findAll().single()).isEqualTo(first)
