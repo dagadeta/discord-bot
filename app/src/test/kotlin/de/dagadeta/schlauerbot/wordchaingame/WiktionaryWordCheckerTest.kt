816a3c27ef95f3219d7c157a88f8b670f4015456
rhaendel <rhaendel@users.noreply.github.com>
2025-05-17 18:53:16 +0200

    #16 Make Logging a Component
    * Now the Logging class can be a Spring-managed component and be injected
      into DiscordBot.
    * The @PreDestroy function is called right before the DiscordBot instance
      is being removed by the container. This makes the shutdown hook on the
      java Runtime class obsolete (wich didn't work since the momenten we are
      using Spring Boot).

--- app/src/test/kotlin/de/dagadeta/schlauerbot/wordchaingame/WiktionaryWordCheckerTest.kt
+++ app/src/test/kotlin/de/dagadeta/schlauerbot/wordchaingame/WiktionaryWordCheckerTest.kt
@@ -1,12 +1,13 @@
 package de.dagadeta.schlauerbot.wordchaingame
 
+import de.dagadeta.schlauerbot.config.LoggingConfig
 import de.dagadeta.schlauerbot.discord.Logging
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class WiktionaryWordCheckerTest {
 
-    private val wordChecker = WiktionaryWordChecker("en", Logging.INITIAL)
+    private val wordChecker = WiktionaryWordChecker("en", Logging(null, LoggingConfig(0, 0)))
 
     @Test
     fun `a word can be checked`() {
